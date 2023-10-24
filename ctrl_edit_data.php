f3853f9f9cfafd635677fe7258cd5c87332e07e0
ElGibran17 <ibrahimelgibran17@gmail.com>
2023-10-24 21:59:36 +0700

    update readme.md

--- ctrl_edit_data.php
+++ ctrl_edit_data.php
@@ -14,7 +14,7 @@ if(isset($_POST['edit'], $_GET['nim'])){
     $pr = $_POST['prodi_mahasiswa'];
 
     // buat query update
-    $sql = "UPDATE tb_mahasiswa SET nim_mhs=$nim, nm_mhs='$nm', jdl_mhs='$jdl', des_mhs='$des', pr_mhs='$pr' WHERE nim_mhs='{$_GET['nim']}'";
+    $sql = "UPDATE tb_mahasiswa SET nim_mhs=$nim, nm_mhs='$nm', jdl_mhs='$jdl', des_mhs='$des', pr_mhs='$pr ' WHERE nim_mhs='{$_GET['nim']}'";
     $query = mysqli_query($koneksi, $sql);
 
     // query update berhasil?
